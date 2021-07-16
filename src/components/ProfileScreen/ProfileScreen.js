d207c2fa8cc4bffd34ed6f06396c3d8812cdcf61
sachinABK <57856730+sachinABK@users.noreply.github.com>
2021-07-16 22:56:56 +0530

    adding auth feature

---
+++ src/components/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import { MovieContext } from "../../context/globalState";
+import Navbar from "../Navbar/Navbar";
+import { auth } from "../../firebase";
+import "./ProfileScreen.css";
+const ProfileScreen = () => {
+  const { user } = useContext(MovieContext);
+  return (
+    <div className="profileScreen">
+      <Navbar />
+      <div className="profileScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt=""
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>Plans</h3>
+              <p></p>
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileScreen;
